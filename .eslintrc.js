4f88e4e37c2d423e001cd3dffae132df3104745a
Haben Amare <haben.amare@outlook.com>
2020-04-09 02:32:29 +0300

    remove 'eslint-config-airbnb'

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,26 +9,9 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   extends: [
-    'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
   ],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.mjs', '.js', '.ts', '.json'],
-      },
-    },
-  },
-  rules: {
-    'import/no-unresolved': [
-      'error',
-      {
-        // Temporary fix for errors that occur when importing declaration
-        // files from `@types/*`
-        ignore: ['vscode'],
-      },
-    ],
-  },
+  rules: {},
 };
